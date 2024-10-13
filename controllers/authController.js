dc949648420dcc187f9ee40ae6f31bec0ad69a40
ananthu7025 <ananthapadmanabhan7025@gmail.com>
2024-10-14 01:43:43 +0530

    auth expirey

--- controllers/authController.js
+++ controllers/authController.js
@@ -33,7 +33,7 @@ exports.login = async (req, res, next) => {
         const isMatch = await bcrypt.compare(password, user.password);
         if (!isMatch) return res.status(401).json({ message: 'Invalid credentials' });
 
-        const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: '1h' });
+        const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: '10h' });
         res.status(200).json({ 
             token,
             saveStatus: true // Indicating successful login
