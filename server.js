3ab0d2d028e2e84c78dc6314ca3509ea28b4c12b
ananthu7025 <93914698+ananthu7025@users.noreply.github.com>
2025-07-02 19:35:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 // MongoDB connection
-mongoose.connect(process.env.MONGO_URIATLAS)
+mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('MongoDB connected'))
     .catch((err) => console.error('MongoDB connection error:', err));
 
